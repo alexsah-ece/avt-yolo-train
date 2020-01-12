ec4dd086b1f4bf0a737cf40556ad0a327e611f0c
Alexandros Sahinis <alexsah97@gmail.com>
2020-01-12 18:23:13 +0200

    Add script for random dataset sampling

---
+++ split_train_val.py
@@ -0,0 +1,20 @@
+import glob
+import os
+import numpy as np
+import sys
+current_dir = "./data/nba/images"
+split_pct = 10  # 10% validation set
+file_train = open("data/nba/train.txt", "w")  
+file_val = open("data/nba/val.txt", "w")  
+counter = 1  
+index_test = round(100 / split_pct)  
+for fullpath in glob.iglob(os.path.join(current_dir, "*.jpg")):  
+  title, ext = os.path.splitext(os.path.basename(fullpath))
+  if counter == index_test:
+    counter = 1
+    file_val.write(current_dir + "/" + title + '.jpg' + "\n")
+  else:
+    file_train.write(current_dir + "/" + title + '.jpg' + "\n")
+    counter = counter + 1
+file_train.close()
+file_val.close()
